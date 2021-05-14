8b8cf856ccea7cafef38717def7370003de9ab90
hitman <mrezaee79@gmail.com>
2021-05-14 05:58:39 -0700

    add git

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style.main">
+    <h1>helow world!</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" module>
+.main {
+  width: 100%;
+  background: #165432;
+  height: 400px;
+  h1 {
+    font-size: 40px;
+    color: #fff;
+    text-align: center;
+  }
+}
+</style>
